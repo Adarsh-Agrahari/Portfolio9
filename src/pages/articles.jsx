5a21cdccbb65263adeb58bd327319f8af0f66605
Adarsh Agrahari <adarshagrahari0503@gmail.com>
2024-12-09 05:27:10 +0530

    Update article page

--- src/pages/articles.jsx
+++ src/pages/articles.jsx
@@ -8,14 +8,6 @@ export default function Articles({ articles = [] }) {
 		<div className={styles.articleContainer}>
 			<Head>
 				<title>Manoj Bhoyar</title>
-				<meta
-					name="description"
-					content="Explore articles on topics like AI, cloud computing, ERP integration, and more. Authored by a seasoned professional with expertise in Oracle ERP and emerging technologies."
-				/>
-				<meta
-					name="keywords"
-					content="AI, Oracle ERP, Cloud Computing, Machine Learning, Big Data, Articles, Research"
-				/>
 				<meta name="author" content="Your Name" />
 			</Head>
 			<div className={styles.articleContent}>
@@ -61,64 +53,88 @@ export async function getServerSideProps() {
 	try {
 		const articles = [
 			{
-				title: "AI-Powered Edge Computing in Cloud Ecosystems: Enhancing Latency Reduction and Real-Time Decision-Making in Distributed Networks",
-				link: "https://welltestingjournal.com/index.php/WT/article/view/109",
-				year: 2024,
-				image: "img/paper9.jpg",
+				title: "Optimizing Database Replication Strategies through Machine Learning for Enhanced Fault Tolerance in Cloud-Based Environments",
+				link: "https://revistadecineforum.com/index.php/cf/article/view/178",
+				year: 2023,
+				image: "img/paper/paper1.jpg",
 			},
 			{
 				title: "Autonomous Test Oracles: Integrating AI for Intelligent Decision-Making in Automated Software Testing",
 				link: "https://welltestingjournal.com/index.php/WT/article/view/108",
 				year: 2024,
-				image: "img/paper8.jpg",
+				image: "img/paper/paper2.jpg",
 			},
 			{
-				title: "Exploring The Ethical Implications Of Ai In Data Analytics: Challenges And Strategies For Responsible Implementation",
-				link: "https://ijcrt.org/papers/IJCRT2309747.pdf",
-				year: 2023,
-				image: "img/paper6.jpg",
+				title: "Decentralized machine learning model orchestration in distributed cloud environments: A meta-learning framework integrating Artificial Intelligence for predictive resource allocation ",
+				link: "https://wjarr.com/sites/default/files/WJARR-2019-0029.pdf",
+				year: 2019,
+				image: "img/paper/paper3.jpg",
 			},
 			{
-				title: "Optimizing Database Replication Strategies through Machine Learning for Enhanced Fault Tolerance in Cloud-Based Environments",
-				link: "https://revistadecineforum.com/index.php/cf/article/view/178",
-				year: 2023,
-				image: "img/paper7.jpg",
+				title: "AI-Powered Edge Computing in Cloud Ecosystems: Enhancing Latency Reduction and Real-Time Decision-Making in Distributed Networks",
+				link: "https://welltestingjournal.com/index.php/WT/article/view/109",
+				year: 2024,
+				image: "img/paper/paper4.jpg",
+			},
+			{
+				title: "The Integration of Data Engineering and Cloud Computing in the Age of Machine Learning and Artificial Intelligence",
+				link: "https://www.irejournals.com/formatedpaper/1700696.pdf",
+				year: 2018,
+				image: "img/paper/paper5.jpg",
+			},
+			{
+				title: "Integrating AI for Intelligent Automation: A Paradigm Shift in Software Engineering Practices for Optimizing Development Lifecycles",
+				link: "https://www.ijaresm.com/uploaded_files/document_file/Manoj_BhoyarYNj4.pdf",
+				year: 2021,
+				image: "img/paper/paper6.jpg",
+			},
+			{
+				title: "A Hybrid Approach to Big Data Analytics: Integrating Cloud Computing with Distributed Machine Learning Algorithms",
+				link: "https://erpublications.com/uploaded_files/download/manoj-bhoyar_WdjAS.pdf",
+				year: 2020,
+				image: "img/paper/paper7.jpg",
+			},
+			{
+				title: "AI-Driven Predictive Maintenance in IoT-Enabled Industrial Systems",
+				link: "https://www.irejournals.com/formatedpaper/1701235.pdf",
+				year: 2019,
+				image: "img/paper/paper8.jpg",
+			},
+			{
+				title: "Next-Generation Cloud Computing: Advancements in Distributed Architectures, AI-Orchestrated Workloads, and Autonomous Multi-Cloud Management for Enhanced Operational Efficiency",
+				link: "https://ijcrt.org/papers/IJCRT2211621.pdf",
+				year: 2022,
+				image: "img/paper/paper9.jpg",
 			},
 			{
 				title: "Federated Learning for Privacy-Preserving AML in Multi-Bank Collaborations",
 				link: "https://tijer.org/tijer/papers/TIJER2311128.pdf",
 				year: 2023,
-				image: "img/paper10.jpg",
+				image: "img/paper/paper10.jpg",
 			},
 			{
-				title: "Evaluating The Impact Of Erp Cloud Integration On Organizational Agility And Decision-Making",
-				link: "https://ijcrt.org/papers/IJCRT2106901.pdf",
+				title: "Advancing Natural Language Processing with Generative AI: A Framework for Contextual Understanding and Automated Content Creation",
+				link: "https://www.irjet.net/archives/V8/i10/IRJET-V8I10312.pdf",
 				year: 2021,
-				image: "img/paper2.jpg",
+				image: "img/paper/paper11.jpg",
 			},
 			{
-				title: "Advancements In Deep Learning Architectures: A Comparative Study Of Performance Metrics And Applications In Real-World Scenarios",
-				link: "https://ijcrt.org/papers/IJCRT2110463.pdf",
-				year: 2021,
-				image: "img/paper3.jpg",
+				title: "Enhancing Conversational Agents with Generative AI: A Framework for Creating More Adaptive and Context-aware chatbots",
+				link: "https://www.irjet.net/archives/V10/i10/IRJET-V10I10132.pdf",
+				year: 2023,
+				image: "img/paper/paper12.jpg",
 			},
 			{
-				title: "Harness Oracle Cloud Infrastructure for Scalable AI Solutions: A Study on Performance and Cost Efficiency",
-				link: "https://tijer.org/tijer/papers/TIJER2111004.pdf",
-				year: 2021,
-				image: "img/paper4.jpg",
+				title: "AUTOMATED ANOMALY DETECTION IN BIG DATA USING DEEP LEARNING AND ADVANCED DATA MINING TECHNIQUES",
+				link: "https://tijer.org/tijer/papers/TIJER2411079.pdf",
+				year: 2024,
+				image: "img/paper/paper13.jpg",
 			},
 			{
-				title: "Integrating Machine Learning Algorithms in Big Data Analytics: A Framework for Enhancing Predictive Insights",
-				link: "https://www.ijaresm.com/uploaded_files/document_file/Swetha_ChintaBTSS.pdf",
-				year: 2021,
-				image: "img/paper5.jpg",
-			},
-			{
-				title: "The role of generative AI in oracle database automation: Revolutionizing data management and analytics",
-				link: "https://wjarr.com/content/role-generative-ai-oracle-database-automation-revolutionizing-data-management-and-analytics",
-				year: 2019,
-				image: "img/paper1.jpg",
+				title: "Hybrid Cloud-Edge Architectures for Low-Latency IoT Machine Learning",
+				link: "https://www.ijirset.com/upload/2023/march/4_Hybrid.pdf",
+				year: 2023,
+				image: "img/paper/paper14.jpg",
 			},
 		];
 		return {
